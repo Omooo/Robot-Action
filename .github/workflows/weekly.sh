0f4173e6cfc76c8dc48f99fafa074f387c9a9944
Omooo <869759698@qq.com>
2022-12-01 19:55:00 +0800

    Create weekly.sh

---
+++ .github/workflows/weekly.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl -X POST -H "Content-Type: application/json" \
+        -d '{"msg_type":"text","content":{"text":"<at user_id = \"all\">All</at> 周报周报～"}}' \
+  https://open.feishu.cn/open-apis/bot/v2/hook/7acb87e2-12d9-43a0-be87-7fd4822a2b07
